655fd82d131a0fad6e876365cfdd54c4337ab8ce
jay <wazurkar@gmail.com>
2013-02-07 16:58:16 -0500

    2nd stupid commit

--- GetType/Program.cs
+++ GetType/Program.cs
@@ -14,6 +14,7 @@ namespace GetType
             object obj = (object) aStringValue;
             Console.WriteLine(obj.GetType().ToString());
             Console.ReadLine();
+            Console.WriteLine();
         }
     }
 }
